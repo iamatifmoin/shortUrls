c12adfc9f0e50a5fa6848b58e108da38e1a09c55
iamatifmoin <atifmoin3@gmail.com>
2025-04-13 13:42:45 +0530

    fixed deployment link

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ mongoose
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://tinieurlz.vercel.app/"],
+    origin: ["https://tinieurlz.vercel.app"],
     methods: ["GET", "POST", "DELETE"],
     credentials: true,
   })
